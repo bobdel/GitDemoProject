6ca2e09d0537c5a3237a687a27599f786d481a62
5385825+bobdel@users.noreply.github.com <5385825+bobdel@users.noreply.github.com>
2024-07-15 18:54:36 -0700

    Initial Commit

---
+++ GitDemoProject/GitDemoProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitDemoProjectApp.swift
+//  GitDemoProject
+//
+//  Created by Robert DeLaurentis on 7/15/24.
+//
+
+import SwiftUI
+
+@main
+struct GitDemoProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
